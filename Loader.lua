d127293d1e0a47214564e3b83c881a21ce7d5a84
dimziexx <newezxx@outlook.com>
2025-06-18 20:11:13 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("Empty"))
+local kz, qx = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/dimziexx/InprisationDev/refs/heads/main/Core/Main/ESP.lua"))()
+end)
+
+if not kz then
+    warn("Error loading ESP script: " .. tostring(qx))
+end
